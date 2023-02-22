e3bfb7ccbd338773f2837bb8412bfc63694873ea
Lorenzo Boldrini <84092873+BoldriniLorenzo@users.noreply.github.com>
2023-02-22 21:58:31 +0100

    Revert "png and audio shadowjoe UPDATE"
    This reverts commit e655274f924403db3eba09ab2e2270eea960755c.

--- ONASproj/Assets/ShadowBehaviour.cs
+++ ONASproj/Assets/ShadowBehaviour.cs
@@ -9,7 +9,6 @@ public class ShadowBehaviour : MonoBehaviour
     public float downTime = 10f;
     public bool ShadowActive;
     public bool Done = false;
-    public AudioSource labAlarm;
     public GameObject ShadowJoeAppereance;
     public GameObject redLights;
     public void Start()
@@ -37,12 +36,10 @@ public class ShadowBehaviour : MonoBehaviour
         GameObject.FindWithTag("DoorManager").GetComponent<DoorManager>().OpenDoorsOffice();
         GameObject.FindWithTag("GameManager").GetComponent<GameManager>().OpenCamera();
         redLights.SetActive(true);
-        labAlarm.Play();
     }
     public void UnlockDoors() {
         GameObject.FindWithTag("DoorManager").GetComponent<DoorManager>().LockDoorsChange();
         redLights.SetActive(false);
-        labAlarm.Stop();
         Done = false;
     }
 

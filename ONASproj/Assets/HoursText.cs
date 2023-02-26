363d81df16e24d5f97903dac55a2c8a1f117b431
Acquahbot <111962430+Acquahbot@users.noreply.github.com>
2023-02-26 17:52:03 +0100

    BILANCIAMENTO NOTTE

--- ONASproj/Assets/HoursText.cs
+++ ONASproj/Assets/HoursText.cs
@@ -27,22 +27,42 @@ public class HoursText : MonoBehaviour
 
     public void AM1() {
         hoursText.text = "1 AM";
+        GameObject.FindWithTag("GameManager").GetComponent<GameManager>().ScubaJoeAILevel = 6;
+        GameObject.FindWithTag("GameManager").GetComponent<GameManager>().TemporaryAILevel = 4;
+        GameObject.FindWithTag("GameManager").GetComponent<GameManager>().AnalJoeAILevel = 3;
+        GameObject.FindWithTag("GameManager").GetComponent<GameManager>().ShadowAILevel = 0;
     }
     public void AM2()
     {
         hoursText.text = "2 AM";
+        GameObject.FindWithTag("GameManager").GetComponent<GameManager>().ScubaJoeAILevel = 10;
+        GameObject.FindWithTag("GameManager").GetComponent<GameManager>().TemporaryAILevel = 8;
+        GameObject.FindWithTag("GameManager").GetComponent<GameManager>().AnalJoeAILevel = 8;
+        GameObject.FindWithTag("GameManager").GetComponent<GameManager>().ShadowAILevel = 4;
     }
     public void AM3()
     {
         hoursText.text = "3 AM";
+        GameObject.FindWithTag("GameManager").GetComponent<GameManager>().ScubaJoeAILevel = 12;
+        GameObject.FindWithTag("GameManager").GetComponent<GameManager>().TemporaryAILevel = 12;
+        GameObject.FindWithTag("GameManager").GetComponent<GameManager>().AnalJoeAILevel = 10;
+        GameObject.FindWithTag("GameManager").GetComponent<GameManager>().ShadowAILevel = 10;
     }
     public void AM4()
     {
         hoursText.text = "4 AM";
+        GameObject.FindWithTag("GameManager").GetComponent<GameManager>().ScubaJoeAILevel = 16;
+        GameObject.FindWithTag("GameManager").GetComponent<GameManager>().TemporaryAILevel = 15;
+        GameObject.FindWithTag("GameManager").GetComponent<GameManager>().AnalJoeAILevel = 15;
+        GameObject.FindWithTag("GameManager").GetComponent<GameManager>().ShadowAILevel = 14;
     }
     public void AM5()
     {
         hoursText.text = "5 AM";
+        GameObject.FindWithTag("GameManager").GetComponent<GameManager>().ScubaJoeAILevel = 18;
+        GameObject.FindWithTag("GameManager").GetComponent<GameManager>().TemporaryAILevel = 15;
+        GameObject.FindWithTag("GameManager").GetComponent<GameManager>().AnalJoeAILevel = 16;
+        GameObject.FindWithTag("GameManager").GetComponent<GameManager>().ShadowAILevel = 15;
     }
     public void AM6()
     {
